720266430f78f25c126712dbfc2e0eebad982a60
Ma5ker <kcahlb@gemail.com>
2018-12-29 17:12:55 +0800

    sdsada

--- code_01/main.cpp
+++ code_01/main.cpp
@@ -5,10 +5,16 @@ class Solution {
 public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		vector<int> ret;
-		for (vector<int>::iterator it = nums.begin(); it < nums.end(); it++) {
-
+		for (vector<int>::iterator it = nums.begin(); it < nums.end()-1; it++) {
+			for (vector<int>::iterator ig = it + 1; ig < nums.end(); ig++) {
+				if (*it + *ig == target) {
+					ret.push_back( it-nums.begin());
+					ret.push_back(ig-nums.begin());
+					return ret;
+				}
+			}
 		}
-		return ret;
+		return {0};
 	}
 };
 
@@ -16,7 +22,7 @@ int main() {
 	vector<int> nums = {2, 7, 10, 11};
 	Solution sl;
 	vector<int> vet = sl.twoSum(nums, 9);
-	for (vector<int>::iterator it = nums.begin(); it < nums.end(); it++) {
-		cout << *it<<endl;
+	for (vector<int>::iterator it = vet.begin(); it < vet.end(); it++) {
+		cout << *it<<"  ";
 	}
 }
---
+++ code_02/mian.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		int a =0, b = 0;
+
+	}
+};
+
+
+int main() {
+
+}
