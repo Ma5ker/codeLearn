d025c7935052ca47f417f4d8fb3e690de070a2ec
Ma5ker <kcahlb@gmail.com>
2019-11-06 11:40:37 +0800

    add 617

---
+++ code_617/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+		if (t1 == NULL) {
+			return t2;
+		}
+		if (t2 == NULL) {
+			return t1;
+		}
+		if (t1 == NULL && t2 == NULL) {
+			return NULL;
+		}
+		t1->val += t2->val;
+		t1->left = mergeTrees(t1->left, t2->left);
+		t1->right = mergeTrees(t1->right, t2->right);
+		return t1;
+	}
+};
+
+
+int main() {
+	//nothing
+}
